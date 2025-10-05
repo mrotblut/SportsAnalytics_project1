5097a970972ab2042c65e399b3c6039d38fbdcfb
mrotblut <123139527+mrotblut@users.noreply.github.com>
2025-10-05 12:35:04 -0500

    Update CFBRushWar.R

--- CFBRushWar.R
+++ CFBRushWar.R
@@ -1,6 +1,8 @@
 library(cfbfastR)
 library(espnscrapeR)
 library(tidyverse)
+seasons = 2024
+
 
 pbp <- load_cfb_pbp(seasons = 2025)
 
@@ -26,43 +28,45 @@ colnames(pbp)
 
 
 # Points to win
-# Uses only games where both teams are fbs or fcs and where both teams don't have a score of zero
-games <- load_cfb_schedules(2024) %>% 
+# Uses only games where both teams are fcs and where both teams don't have a score of zero (canceled)
+games = load_cfb_schedules(seasons) %>% 
   filter(season_type == "regular", 
-         home_division=='fbs' | home_division == 'fcs', 
-         away_division=='fbs' | away_division == 'fcs', 
-         !(home_points == 0 & away_points == 0)) %>% 
-  select(season, home_team, home_conference, home_points, away_team, away_conference, away_points) %>% 
+         home_division == 'fcs', 
+         away_division == 'fcs',
+         completed == TRUE,
+         (home_points != 0 | away_points != 0))
+
+conf = games %>% 
+  select(season,home_team,home_conference) %>%
+  rename(team = home_team, conf = home_conference) %>% 
+  distinct(season,team,conf)
+
+games = games %>% 
   mutate(result = home_points-away_points,
          win_t = ifelse(result > 0, home_team, away_team),
          lose_t = ifelse(result < 0, home_team, away_team),
          win_by = ifelse(win_t == home_team, result, -1*result),
-         lose_by = ifelse(lose_t == home_team, result, -1*result) )
+         lose_by = ifelse(lose_t == home_team, result, -1*result) ) %>% 
+  select(season,win_t,win_by,lose_t,lose_by,result)
 
-conf <- games %>% 
-  select(home_team,home_conference) %>%
-  rename(team = home_team, conf = home_conference) %>% 
-  distinct(team,conf)
-  
-
-win_diff <-
+win_diff =
   games  %>% 
   dplyr::group_by(season, win_t) %>%
   dplyr::summarise(wins = dplyr::n(), win_diff = sum(win_by), .groups = 'drop') %>%
   dplyr::rename(team = win_t)
 
-loss_diff <-
+loss_diff =
   games %>%
   dplyr::group_by(season, lose_t) %>%
   dplyr::summarise(loss = dplyr::n(), loss_diff = sum(lose_by), .groups = 'drop') %>%
   dplyr::rename(team = lose_t)
 
-records <-
+records =
   win_diff %>%
   dplyr::full_join(y = loss_diff, by = c("season", "team")) %>%
   dplyr::mutate(across(everything(), ~replace_na(.x,0)),
                 scoring_diff = win_diff + loss_diff) %>% 
-  inner_join(conf)
+  left_join(conf, by = c("season","team"))
 
-win_score_fit <- lm(wins~scoring_diff, data = records)
-points_to_win <- coefficients(win_score_fit)[2] %>% unname()
+win_score_fit = lm(wins~scoring_diff, data = records)
+points_to_win = coefficients(win_score_fit)[2] %>% unname()
